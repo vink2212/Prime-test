05ede683bbc97d4bde403b70ca73b04268260cbb
Vinayak Sehgal <62817618+vink2212@users.noreply.github.com>
2022-04-11 03:58:44 -0700

    Update prime.py
    Added comments to better explain and organize the code. Added winsound for extra fun when the program is done running.

--- prime.py
+++ prime.py
@@ -1,39 +1,41 @@
 print("program start")
 from math import floor
 
-
+#Checks if a number is prime
 def check_prime(number):
     c = 2
-
+    
     counter = 0
     while c < floor(number**0.5):
         if number % c == 0:
             return False
             counter += 1
             break
-
+        
         c += 1
     if counter > 0:
         return False
     if counter == 0:
         return True
-
-
+        
 def largest_factor(number):
-    n = floor(number**0.5)
+    n = floor(number ** 0.5) #finds sqaure root of number
     i = 0
-    prime = 0
+    prime  = 0
     run = True
-    while run:
-        if check_prime(n - i):
-            if number % (n - i) == 0:
-                prime = n - i
+    while run: #prints largest prime factor by counting down from sqrt of number and checking if its prime.
+        if check_prime(n-i):
+            if number % (n-i) == 0:
+                prime = n-i
                 print(prime)
                 run = False
                 break
-        i += 1
+        i +=1
+        
+largest_factor(600851475143)
 
-
-largest_factor(300)
-
-
+#Plays a sound when program is done running.
+import winsound
+frequency = 2500  # Set Frequency To 2500 Hertz
+duration = 5000  # Set Duration To 1000 ms == 1 second
+winsound.Beep(frequency, duration)
